848ab668ec070ac8770feb2aad3ebd10a7177f17
deniz <deniz@DESKTOP-INS52F1>
2023-12-03 16:09:45 +0300

    Added guid and file helpers

---
+++ Core/Utilities/Helpers/FileHelpers/IFileHelper.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Helpers
+{
+    public interface IFileHelper
+    {
+        void Delete(string filePath);
+        string Update(IFormFile file, string filePath, string root);
+    }
+}
