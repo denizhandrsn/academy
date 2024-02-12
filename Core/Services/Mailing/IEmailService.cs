5d1cbd0188d91c93677ca1023a3681110c7488c0
Denizhan Dursun <denizhan4050@hotmail.com>
2024-02-12 23:30:26 +0300

    Added Mailing Service

---
+++ Core/Services/Mailing/IEmailService.cs
@@ -0,0 +1,15 @@
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.Extensions.Options;
+using MimeKit;
+using MimeKit.Text;
+
+
+namespace Core.Services.Mailing;
+
+public interface IEmailService
+{
+    void Send(string to, string subject, string html, string from = null);
+}
+
+
